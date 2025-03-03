3fec1cd0341aca8bc45f07f65a6fd6ee5d8c8ab8
Mauro <maurodefalco96@gmail.com>
2025-03-04 00:30:25 +0100

    seo meta with Prismic data

---
+++ app/layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    const prismic = usePrismic();
+
+    const { data: settings } = await useAsyncData( () => prismic.client.getSingle('settings'));
+
+    useSeoMeta({
+        title: settings.value?.data.site_title,
+        description: settings.value?.data.meta_description,
+        ogTitle: settings.value?.data.site_title,
+        ogImage: computed(() => prismic.asImageSrc(settings.value?.data.meta_image)),
+    });
+</script>
+
+
+<template>
+    <div>
+        {{ settings }}
+        <slot />
+    </div>
+</template>
+
+
