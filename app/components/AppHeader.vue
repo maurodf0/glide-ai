baf96c12090b7344e889d958667d3e918d63b793
Mauro <maurodefalco96@gmail.com>
2025-03-04 00:54:43 +0100

    header

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -7,8 +7,17 @@ import type { Content } from '@prismicio/client'
 
 
 <template>
-    <header class="py-4 max-w-screen-md mx-auto">
-      <GlideLogo />
+    <header class="p-4 md:p-6 flex items-center justify-between">
+        <nav aria-label="Main" class="flex justify-between items-center w-10/12">
+            <NuxtLink to="/" class="flex items-center">
+                <GlideLogo />
+            </NuxtLink>
+            <ul class="hidden md:flex items-center gap-6">
+                <li v-for="link in settings?.data.navigation" :key="link.key">
+                    <PrismicLink :field="link" :class="link.variant" />
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
