8e03adf65b0de9d238479eccee6f4be9d634fceb
Mauro <maurodefalco96@gmail.com>
2025-03-10 23:42:43 +0100

    Aggiungi componente Bounded con supporto per props e slot

---
+++ app/components/Bounded.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    defineProps<{as?: string}>()
+</script>
+
+<template>
+ <Component :is="as || 'section'"
+    class="px-4 py-14 first:!pt-10" />
+    <div class="mx-auto w-full max-w-6xl flex-col items-center">
+        <slot/>
+    </div>
+  </template>
