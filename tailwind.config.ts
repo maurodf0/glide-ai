30d84032ac30465d1fd5605eb2936f88debf3eb8
Mauro <maurodefalco96@gmail.com>
2025-03-11 23:59:41 +0100

    Rimuovi il file di configurazione Tailwind e aggiorna la configurazione in TypeScript per includere i percorsi dei componenti

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { ModuleOptions } from "@nuxtjs/tailwindcss";
 
-export default <Partial<Config>>{
-    mode: "jit",
-  content: ['/app/**/*.{js,ts,vue}']
-}
+const tailwindConfig: ModuleOptions = {
+  config: {
+    content: [
+      "./components/**/*.{vue,js,ts}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./app.vue",
+      "./plugins/**/*.{js,ts}",
+      "./slices/**/*.{vue,js,ts}", // Assicurati che gli Slice siano inclusi
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  },
+};
+
+export default tailwindConfig;
