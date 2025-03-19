d9ffdc0db50bbe42eda35268bf68bd677a2c0be0
Mauro <maurodefalco96@gmail.com>
2025-03-20 00:26:40 +0100

    style image by variation

--- app/slices/Showcase/index.vue
+++ app/slices/Showcase/index.vue
@@ -14,10 +14,36 @@ defineProps(
 </script>
 
 <template>
-  <section
+  <Bounded
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    Placeholder component for showcase (variation: {{ slice.variation }}) Slices
-  </section>
+   <PrismicRichText 
+    wrapper="header"
+    class="text-balance text-center text-5xl md:text-7xl font-medium"
+    :field="slice.primary.heading" />
+    <article 
+    class="relative mt-16 grid items-center gap-8 rounded-xl border border-sky-50/20 bg-gradiant-to-b from-gray-50/15 to-gray-50/5 p-8 backdrop-blur-sm lg:grid-cols-3 lg:gap-0 lg:py-12">
+      <div>
+        <figure class="w-fit rounded-lg bg-sky-900 p-4 text-3xl border-gray-100/10 border-4">
+          <Icon :name="slice.primary.icon" class="block" />
+        </figure>
+        <PrismicText 
+          wrapper="h3"
+          :field="slice.primary.subheading" 
+          class="mt-6 text-2xl font-bold" />
+        <PrismicRichText 
+          :field="slice.primary.body" 
+          wrapper="div"
+          class="prose prose-invert mt-4 max-w-xl"/>
+        <PrismicLink 
+          class="buttonLink mt-6"
+          :field="slice.primary.cta" />
+      </div>
+      <PrismicImage 
+        :class="slice.variation === 'reversed' ? 'lg:order-1 lg:translate-x-[15%]' : 'lg:-order-1 lg:translate-x-[-15%]'"
+        class="opacity-90 shadow-2xl lg:col-span-2 lg:pt-0"
+        :field="slice.primary.image" />
+   </article>
+  </Bounded>
 </template>
