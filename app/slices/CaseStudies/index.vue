baa98c815b51643d9ccb1ac07c15c3015ec09e54
Mauro <maurodefalco96@gmail.com>
2025-03-22 19:04:03 +0100

    single case study style

--- app/slices/CaseStudies/index.vue
+++ app/slices/CaseStudies/index.vue
@@ -37,14 +37,43 @@ const caseStudies = computed( () => {
     :field="slice.primary.body" />
     <div class="grid mt-20 gap-16">
     <article 
-      v-for="caseStudy in caseStudies"
+      class="group relative grid gap-4 opacity-85 transition-opacity will-change-auto duration-300 hover:cursor-pointer hover:opacity-100 md:grid-cols-2 lg:grid-cols-3"
+      v-for="(caseStudy, index) in caseStudies"
       :key="caseStudy.id">
-      <PrismicText :field="caseStudy.data.company"/>
-      <PrismicRichText :field="caseStudy.data.description"/>
-      <PrismicImage :field="caseStudy.data.cover"/>
+      <div class="col-span-1 flex flex-col justify-center gap-4">
+        <PrismicText
+        wrapper="h3"
+        class="text-4xl" 
+        :field="caseStudy.data.company"/>
+      <PrismicRichText
+        class="max-w-md"
+        wrapper="div"      
+       :field="caseStudy.data.description"/>
+       <PrismicLink 
+        class="z-10 after:absolute after:inset-0 hover:underline"
+        :document="caseStudy">Read <PrismicText :field="caseStudy.data.company"/></PrismicLink>
+      </div>
+      <div 
+      class="relative lg:col-span-2"
+      :class="index % 2 && 'md:-order-1'" >
+        <div class="imageGlow -bottom-8 left-4 bg-sky-500"></div>
+        <div class="imageGlow -right-4 top-8 bg-teal-500"></div>
+        <PrismicImage 
+        class="z-20 scale-[.98] rounded-xl transition-transform duration-300 will-change-auto group-hover:scale-100"
+        :field="caseStudy.data.cover"/>
+      </div>
     </article>
+    
 </div>
 
   </Bounded>
 </template>
 
+
+<style scoped>
+
+  .imageGlow {
+    @apply absolute h-1/2 w-1/2 rounded-full blur-3xl opacity-0 mix-blend-screen transition-opacity will-change-auto duration-500 group-hover:opacity-50;
+  }
+
+</style>
