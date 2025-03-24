acc0c59ea19fb6b16e9f930b4dd71ab09ce1fe59
Mauro <maurodefalco96@gmail.com>
2025-03-24 23:52:10 +0100

    aggiungi animazioni GSAP per il caricamento della pagina case study

--- app/pages/case-studies/[uid].vue
+++ app/pages/case-studies/[uid].vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { components } from "~/slices";
+import gsap from "gsap";
 
 const prismic = usePrismic();
 const route = useRoute();
@@ -20,6 +21,46 @@ useSeoMeta({
   ogDescription: page.value?.data.meta_description,
   ogImage: computed(() => prismic.asImageSrc(page.value?.data.meta_image)),
 });
+
+onMounted(() => {
+  const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches; 
+  
+  if (reduceMotion) {
+    return;
+  }
+
+  const tl = gsap.timeline();
+
+  tl.fromTo('.case_study__title', {
+    opacity: 0,
+    y: 100,
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: 1.5,
+    ease: 'power3.out',
+  })
+
+  tl.fromTo('.case_study__description', {
+    opacity: 0,
+    y: 100,
+  }, {
+    opacity: 1,
+    y: 0,
+    duration: .5,
+    ease: 'power3.out',
+  }, '-=.5');
+
+    tl.fromTo('.case_study__image', {
+      opacity: 0,
+    }, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      delay: 0.5,
+      ease: 'power3.out',
+    }, '-=.5');
+});
 </script>
 
 <template>
@@ -27,15 +68,19 @@ useSeoMeta({
     <Bounded>
       <header class="relative grid w-full place-items-center text-center">
         <GlideGrid />
-        <h1 class="text-5xl font-medium text-balance md:text-7xl">
-          <PrismicText :field="page?.data.company" />
-          <RichTextGlideText class="block text-lg">Case Study</RichTextGlideText>
-        </h1>
+        <div class="block overflow-hidden">
+          <h1 class="case_study__title text-5xl font-medium text-balance md:text-7xl">
+            <PrismicText :field="page?.data.company" />
+            <RichTextGlideText class="case_study__title block text-lg">Case Study</RichTextGlideText>
+          </h1>
+        </div>
+        <div class="block overflow-hidden">
         <PrismicText 
-          class="max-w-xl mb-4 mt-8 text-lg text-gray-300"
+          class="case_study__description max-w-xl mb-4 mt-8 text-lg text-gray-300"
           :field="page?.data.description" 
           wrapper="p"/>
-          <PrismicImage v-if="prismic.isFilled.image(page?.data.cover)" :field="page?.data.cover" class="max-w-4xl w-full rounded-lg h-full object-cover" /> 
+      </div>
+          <PrismicImage v-if="prismic.isFilled.image(page?.data.cover)" :field="page?.data.cover" class="case_study__image max-w-4xl w-full rounded-lg h-full object-cover" /> 
       </header>
     <SliceZone
       wrapper="div"
